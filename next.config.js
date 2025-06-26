64213832b21af81f2a379fb135576a036ee009ec
Hamdi1433 <mdpremunia@gmail.com>
2025-06-26 18:56:05 +0200

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_KEY: process.env.SUPABASE_KEY,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    SMTP_HOST: process.env.SMTP_HOST,
+    SMTP_PORT: process.env.SMTP_PORT,
+    SMTP_USER: process.env.SMTP_USER,
+    SMTP_PASS: process.env.SMTP_PASS,
+  },
+};
+
+module.exports = nextConfig;
